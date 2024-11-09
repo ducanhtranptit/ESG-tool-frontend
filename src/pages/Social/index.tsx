9f52fe0856147c4fed72bd2374d550d3c37c6d2d
ducanhtranptit <tda.ducanh@gmail.com>
2024-11-09 08:50:51 +0700

    fix bug

--- src/pages/Social/index.tsx
+++ src/pages/Social/index.tsx
@@ -220,7 +220,7 @@ const SocialPage: React.FC = () => {
 		<div>
 			{loading ? (
 				<div
-					className="d-flex justify-content-center align-items-center spinner-grow text-success"
+					className="d-flex justify-content-center align-items-center"
 					style={{ height: "80vh" }}
 				>
 					<Spinner animation="border" role="status">
