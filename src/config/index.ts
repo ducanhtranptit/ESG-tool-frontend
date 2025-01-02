a1ee205f9690fc4f61535d678ab1d3e315580ab9
ducanhtranptit <tda.ducanh@gmail.com>
2025-01-02 08:49:45 +0700

    update code

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const ACCESSTOKEN_KEY = "access_token";
-export const REFRESHTOKEN_KEY = "refresh_token";
+export const ACCESSTOKEN_KEY = "esg_tool.webapp.access_token";
+export const REFRESHTOKEN_KEY = "esg_tool.webapp.refresh_token";
